e6ad763b005ca1956f663a948a95fdc672ddae7a
cs <cs@DESKTOP-CUP1M2C>
2025-05-07 18:39:32 +0300

    Initial commit for CartItem

---
+++ E-commerce.Application/DTOs/CartItem/CartItemDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_commerce.Application.DTOs
+{
+    public class CartItemDTO
+    {
+        public int CartItemID { get; set; }
+        public int UserID { get; set; }
+        public int ProductID { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public DateTime DateAdded { get; set; }
+
+        public decimal TotalPrice => Price * Quantity;
+
+    }
+}
---
+++ E-commerce.Application/Services/ICartItemService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using E_commerce.Application.DTOs;
+
+namespace E_commerce.Application.Interfaces
+{
+    public interface ICartItemService
+    {
+        Task AddToCartAsync(int userId, int productId, int quantity);
+        Task UpdateCartItemQuantityAsync(int cartItemId, int quantity);
+        Task RemoveFromCartAsync(int cartItemId);
+        Task<IEnumerable<CartItemDTO>> GetUserCartItemsAsync(int userId);
+        Task ClearCartAsync(int userId);
+    }
+}
