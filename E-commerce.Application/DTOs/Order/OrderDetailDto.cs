c39b296d4d5f1c11dd5d32327c6d9e670f89fe68
MATRIX <MATRIX@DESKTOP-PQOAC0F>
2025-05-07 14:24:13 +0300

    d

---
+++ E-commerce.Application/DTOs/Order/OrderDetailDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_commerce.Application.DTOs.Order
+{
+    public class OrderDetailDto
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ E-commerce.Application/DTOs/Order/OrderDisDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_commerce.Application.DTOs.Order
+{
+    public class OrderDisDto
+    {
+        public int OrderID { get; set; }
+        public int UserID { get; set; } // foreign key
+        public DateTime OrderDate { get; set; }
+        public decimal TotalAmount { get; set; }
+        public string Status { get; set; } // status of the order
+        public DateTime DateProcessed { get; set; } // date when the order was processed
+        public List<OrderDetailDto> OrderDetails { get; set; } // collection navigation property
+    }
+}
