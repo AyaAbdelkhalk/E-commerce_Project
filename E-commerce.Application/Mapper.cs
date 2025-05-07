ac034408c892e92966df25d02e1acaa4f9d855ee
Aya Abdelkhalk <aya.abdelkhalk30@gmail.com>
2025-05-07 14:19:33 +0300

    Mapster

--- E-commerce.Application/Mapper.cs
+++ E-commerce.Application/Mapper.cs
@@ -1,7 +1,27 @@
-﻿namespace E_commerce.Application
+﻿using E_commerce.Application.DTOs.UserDTOs;
+using E_commerce.Core.Models;
+using Mapster;
+
+namespace E_commerce.Application
 {
     public class Mapper
     {
+        public static void RegisterMapsterConfiguration()
+        {
+            //<source, destination>
 
+            #region User Mapping
+            TypeAdapterConfig<AddUserDTO, User>
+                    .NewConfig()
+                    .Map(dest => dest.Password, src => src.Password);
+
+            TypeAdapterConfig<User, UserDetails>
+                .NewConfig();
+
+            #endregion
+
+
+
+        }
     }
 }
---
+++ E-commerce.Infrastructure/Repository/UserRepository.cs
@@ -0,0 +1,25 @@
+﻿using E_commerce.Application.Interfaces;
+using E_commerce.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_commerce.Infrastructure.Repository
+{
+    public class UserRepository : GenericRepository<User>, IUserRepository
+    {
+        public UserRepository(AppDbContext context) : base(context)
+        {
+
+        }
+        public async Task<User> GetByEmailAsync(string email)
+        {
+            var user = await _dbSet.FirstOrDefaultAsync(u => u.Email.ToLower() == email.ToLower());
+            return user;
+        }
+
+    }
+}
