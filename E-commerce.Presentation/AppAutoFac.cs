2aa32f9befc7e6e7f18ecc2bdd317ff30ed36ecb
Aya Abdelkhalk <aya.abdelkhalk30@gmail.com>
2025-05-08 10:06:30 +0300

    Add Layer

---
+++ E-commerce.Presentation/AppAutoFac.cs
@@ -0,0 +1,26 @@
+using Autofac;
+using E_commerce.Application.Interfaces;
+using E_commerce.Application.Services;
+using E_commerce.Infrastructure;
+using E_commerce.Infrastructure.Repository;
+
+namespace E_commerce.Presentation
+{
+    public class AppAutoFac
+    {
+        public static Autofac.IContainer Inject()
+        {
+            var builder = new ContainerBuilder();
+            // Register your services here
+            // Example: builder.RegisterType<MyService>().As<IMyService>();
+            builder.RegisterType<AppDbContext>().AsSelf();
+            builder.RegisterType<UserRepository>().As<IUserRepository>();
+            builder.RegisterType<UserServices>().As<IUserServices>();
+
+
+
+            var container = builder.Build();
+            return container;
+        }
+    }
+}
