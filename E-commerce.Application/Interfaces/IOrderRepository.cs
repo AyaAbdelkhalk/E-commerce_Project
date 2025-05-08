1251d5d93c68ea31bbe2b959dd4e9ba7105b5fe8
MATRIX <MATRIX@DESKTOP-PQOAC0F>
2025-05-08 16:02:18 +0300

    dd

---
+++ E-commerce.Application/Interfaces/IOrderRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_commerce.Application.Interfaces
+{
+    public class IOrderRepository:IGenericRepository<Order>
+    {
+        Task<IQueryable<Order>> GetOrdersByUserIdAsync(int userId);
+
+    }
+}
---
+++ E-commerce.Infrastructure/Repository/OrderRepository .cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_commerce.Infrastructure.Repository
+{
+    public  class OrderRepository: GenericRepository<Order>, IOrderRepository
+    {
+        public OrderRepository(AppDbContext context) : base(context)
+        {
+        }
+
+        public async Task<IEnumerable<Order>> GetOrdersByUserIdAsync(int userId)
+        {
+            return await _dbSet
+                .Include(o => o.OrderItems)
+                .ThenInclude(oi => oi.Product)
+                .Where(o => o.UserID == userId)
+                .ToListAsync();
+
+
+        }
+    }
+   
+}
